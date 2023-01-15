c42d50b27d851d17af7d34a5ec530869d25be9ab
Mathieu Roberts <mathieulroberts@gmail.com>
2023-01-14 18:47:37 -0800

    completed code challenge 5

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,15 +1,37 @@
 class LinkedList:
     """
-    Put docstring here
+    Class that creates a linked list
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def __str__(self):
+        current = self.head
+        string_values = ""
+        while current is not None:
+            string_values += f"{{ {str(current.value)} }} -> "
+            current = current.next
+        string_values += "NULL"
+        return string_values
+
+    def insert(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+
+    def includes(self, value):
+        current = self.head
+        while current is not None:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
 
 
 class TargetError:
---
+++ python/docs/linked_list/README.md
@@ -0,0 +1,18 @@
+# Singly Linked List
+Created a linked list class that can make a linked list with the functionality to add values and print all the values of the linked list and what each node points to
+
+## Challenge
+Create a linked list class
+
+## Approach & Efficiency
+
+wrote a very basic linked list class following tutorials. big O for time is O(1) because it is independent of the size of the input and for space its O(n) for the includes function
+
+
+## API
+
+insert(value) - Adds a new node with that value to the head of the list with an O(1) Time performance.
+
+includes(value) - returns true/false whether that value exists as a Node’s value somewhere within the list.
+
+str() - Returns: a string representing all the values in the Linked List, formatted as:"{ a } -> { b } -> { c } -> NULL"
