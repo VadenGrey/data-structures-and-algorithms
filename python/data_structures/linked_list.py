1964c1c654d357983d9c3ec56efd5b469d7ec0ad
Mathieu Roberts <mathieulroberts@gmail.com>
2023-02-08 19:38:04 -0800

    code challenge 6 completed

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -6,19 +6,55 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+
     def __str__(self):
         current = self.head
-        string_values = ""
+        stringval = ""
         while current is not None:
-            string_values += f"{{ {str(current.value)} }} -> "
+            stringval += f"{{ {str(current.value)} }} -> "
             current = current.next
-        string_values += "NULL"
-        return string_values
+        stringval += "NULL"
+        return stringval
+
 
     def insert(self, value):
-        new_node = Node(value)
-        new_node.next = self.head
-        self.head = new_node
+        self.head = Node(value, self.head)
+
+
+    def append(self, value):
+        current = self.head
+        while current.next is not None:
+            current = current.next
+        current.next = Node(value)
+
+
+    def insert_before(self, before, value):
+        try:
+            current = self.head
+            if current.value == before:
+                self.head = Node(value, current)
+            else:
+                while current.value != before:
+                    previous = current
+                    current = current.next
+                previous.next = Node(value, current)
+        except Exception as e:
+            raise TargetError(e)
+
+
+    def insert_after(self, after, value):
+        try:
+            current = self.head
+            after_val = self.head.next
+            if current.value == after:
+                self.head.next = Node(value, after_val)
+            else:
+                while current.value != after:
+                    current = current.next
+                current.next = Node(value, after_val)
+        except Exception as e:
+            raise TargetError(e)
+
 
     def includes(self, value):
         current = self.head
@@ -34,5 +70,5 @@ class Node:
         self.next = next
 
 
-class TargetError:
+class TargetError(Exception):
     pass
---
+++ python/docs/linked_list_insertions/README.md
@@ -0,0 +1,8 @@
+# Add Insertion functionality to a linked list
+Added functions: append, insert_before, and insert_after to the linked list class
+
+## Whiteboard Process
+![whiteboard](./code6.png)
+
+## Approach & Efficiency
+All the functions added have a Big O time of O(n) because the loop through the list to either find the end or value that was passed in
